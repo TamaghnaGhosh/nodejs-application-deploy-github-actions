c3c653a682a7bb0a3232c45870cb8ba797224151
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2026-08-21 01:13:38 +0530

    fix server host for docker

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT ?? 8080;
+// import express from "express";
+// const app = express();
+// const PORT = process.env.PORT ?? 8080;
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello From the server' });
+// app.get('/', (req, res) => {
+//     return res.json({ message: 'Hello From the server' });
+// });
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Taskflow API is running");
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(8080, "0.0.0.0", () => {
+  console.log("Server running on port 8080");
 });
