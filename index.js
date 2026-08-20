e8fc24e5a1c33e8600b29e060180bed0663c2f0b
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2026-08-21 01:21:58 +0530

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-// import express from "express";
-// const app = express();
-// const PORT = process.env.PORT ?? 8080;
-
-// app.get('/', (req, res) => {
-//     return res.json({ message: 'Hello From the server' });
-// });
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-
-const express = require("express");
+import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Taskflow API is running");
 });
 
-app.listen(8080, "0.0.0.0", () => {
-  console.log("Server running on port 8080");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
